55688696abed90bd0b2390765afde54687a546bc
dshahi <dipu_it_86@yahoo.com>
2015-10-21 20:47:28 -0700

    2nd commit

--- src/test/java/com/appium/facebook/appiumtest/AppTest.java
+++ src/test/java/com/appium/facebook/appiumtest/AppTest.java
@@ -1,38 +1,57 @@
 package com.appium.facebook.appiumtest;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
+
+
 public class AppTest 
-    extends TestCase
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+AppiumDriver dr;
+	
+	
+	@BeforeClass
+	public void appiumSetup() throws MalformedURLException {
+		
+	    DesiredCapabilities  cap=new DesiredCapabilities();
+	    cap.setCapability(CapabilityType.BROWSER_NAME, "");
+	    cap.setCapability("deviceName", "Xperia SP");
+	    cap.setCapability("platformVersion","4.3");
+	    cap.setCapability("platformName","Android");
+	 
+	    
+	    cap.setCapability("appPackage","com.android.calendar");
+	    cap.setCapability("appActivity","LaunchActivity");
+	    
+	    dr=new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+	    
+	    dr.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		
+	}
+	
+	
+	@Test
+	public void LongPressTest(){
+		
+		TouchAction act=new TouchAction(dr);
+		
+		act.longPress(576,630).perform();
+	}
+	
+	
+	@AfterTest
+	public void closeDriver(){
+		dr.quit();
+	}
 }
