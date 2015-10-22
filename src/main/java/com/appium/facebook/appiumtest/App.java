d3f7d78fe54f4f23a15038528a162581e49ca096
dshahi <dipu_it_86@yahoo.com>
2015-10-21 20:20:58 -0700

    first commit

---
+++ src/main/java/com/appium/facebook/appiumtest/App.java
@@ -0,0 +1,13 @@
+package com.appium.facebook.appiumtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
